0a61ced10fa67940728181c3147b53f5da8c978d
Tímea Tamasi <103537849+DoomP1nky@users.noreply.github.com>
2023-02-07 14:26:49 +0100

    add script.js for DOM manipulate, folder structure change, add scss

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+//new staticmiddle app.use(express.static('mappaelérés: `${__dirname}/../frontend'))
+app.use(express.static(`${__dirname}/../frontend`))
 
 const port = 9000;
 
