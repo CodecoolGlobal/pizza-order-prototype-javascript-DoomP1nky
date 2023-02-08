3bb9b15058d7725c1395f686a8a9e769af500051
kurucsai_gyorgy <113708515+kurucsaigyorgy@users.noreply.github.com>
2023-02-08 13:17:55 +0100

    Update json and render images on landing page

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 No starter code is provided. Start from scratch! Do not scratch yourself or others!
+
+let shoeCount = 0;
+function shoeComponent(shoe) {
+    shoeCount++;
+    return `
+    <img id=imgID${shoeCount} src="pictures/Adidas Retropy Adisuper W.jpg">
+    <div>${shoe.name}</div>
+    <div>${shoe.price} Ft</div>
+    `
--- frontend/script.js
+++ frontend/script.js
@@ -9,10 +9,18 @@ const menButton = document.createElement('button');
 menButton.textContent = "MEN'S";
 root.appendChild(menButton);
 
-const selectSize = document.createElement()
-
+// const selectSize = document.createElement()
+let shoeCount = 0;
 function shoeComponent(shoe) {
-    return `<div>${shoe.name}<div>${shoe.price} Ft</div></div>`
+    shoeCount++;
+    return `
+    <br>
+    <div id=contID${shoeCount}>
+        <img id=imgID${shoeCount} src=${shoe.src} width=200 height=200>
+        <div>${shoe.name}</div>
+        <div>${shoe.price} Ft</div>
+    </div>
+    `
 }
 async function initializePage() {
     const response = await fetch(`http://127.0.0.1:9000/api/shoes`);
@@ -21,16 +29,16 @@ async function initializePage() {
 
     const htmlShoes = theShoes.shoes.map(shoe => shoeComponent(shoe))
     const joinedStrings = htmlShoes.join("");
-    const root = document.getElementById('root');
+    // const root = document.getElementById('root');
     root.insertAdjacentHTML('beforeend', joinedStrings)
 
-    womenButton.addEventListener("click", function () {        
-       console.log(theShoes.shoes.filter(shoe=> shoe.gender === "woman"))
+    womenButton.addEventListener("click", function () {
+        console.log(theShoes.shoes.filter(shoe => shoe.gender === "woman"))
     })
 
-    menButton.addEventListener("click", function () {        
-        console.log(theShoes.shoes.filter(shoe=> shoe.gender === "man"))
-     })
+    menButton.addEventListener("click", function () {
+        console.log(theShoes.shoes.filter(shoe => shoe.gender === "man"))
+    })
 
 
 }
