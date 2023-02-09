484c4a766217915c83069dace9dab70445f209a2
kurucsai_gyorgy <113708515+kurucsaigyorgy@users.noreply.github.com>
2023-02-09 09:46:46 +0100

    rework code + size select options

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,13 @@
-console.log("Ez a srcipt file!")
 
+// const header = document.createElement('div')
+// header.id = "header";
+// header.textContent = 'ShoeSzter';
+// //header.style.background = "frontend\pictures\header_pic_v2.jpg";
+// root.appendChild(header)
 const root = document.getElementById('root');
+
+let shoeCount = 0;
+
 const womenButton = document.createElement('button');
 womenButton.textContent = "WOMEN'S";
 root.appendChild(womenButton);
@@ -9,8 +16,15 @@ const menButton = document.createElement('button');
 menButton.textContent = "MEN'S";
 root.appendChild(menButton);
 
-// const selectSize = document.createElement()
-let shoeCount = 0;
+const resetButton = document.createElement('button');
+resetButton.textContent = "RESET";
+root.appendChild(resetButton);
+
+const mainContainer = document.createElement('div');
+mainContainer.id = 'mainCont';
+root.appendChild(mainContainer);
+
+
 function shoeComponent(shoe) {
     shoeCount++;
     return `
@@ -19,34 +33,58 @@ function shoeComponent(shoe) {
         <img id=imgID${shoeCount} src=${shoe.src} width=200 height=200>
         <div>${shoe.name}</div>
         <div>${shoe.price} Ft</div>
+        <label for="size-select">Válasszon méretet:</label>
+        <select name="sizes" id="size-select-${shoeCount}"></select>
     </div>
     `
 }
+function createOptions(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        const currentButton = document.getElementById(`size-select-${i + 1}`)
+        for (let j = 0; j < arr[i].size.length; j++) {
+            const option = document.createElement('option')
+            option.innerText = arr[i].size[j];
+            option.value = arr[i].size[j];
+            currentButton.appendChild(option);
+        }
+    }
+}
+function removeHideClass() {
+    const shoeContainer = document.querySelectorAll('.hideshoes');
+    shoeContainer.forEach((element) => {
+        element.classList.remove('hideshoes');
+    });
+}
+function genderFilter(gender, arr) {
+    removeHideClass();
+    let filteredIDs = [];
+    const allDivs = document.querySelectorAll('div');
+    const filteredShoes = arr.filter(shoe => shoe.gender === gender).map(shoe => filteredIDs.push(shoe.id));
+    for (let i = 0; i < filteredIDs.length; i++) {
+        for (let j = 0; j < allDivs.length; j++) {
+            if (allDivs[j].id.includes(`contID${filteredIDs[i]}`)) {
+                allDivs[j].classList.add('hideshoes');
+            }
+        }
+    }
+}
 async function initializePage() {
+    const gender = ['woman', 'man'];
     const response = await fetch(`http://127.0.0.1:9000/api/shoes`);
     const theShoes = await response.json();
-    console.log(theShoes)
+    const shoesArray = theShoes.shoes;
+    const htmlShoes = shoesArray.map(shoe => shoeComponent(shoe)).join('')
+    mainContainer.insertAdjacentHTML('beforeend', htmlShoes);
+    createOptions(shoesArray);
 
-    const htmlShoes = theShoes.shoes.map(shoe => shoeComponent(shoe))
-    const joinedStrings = htmlShoes.join("");
-    // const root = document.getElementById('root');
-    root.insertAdjacentHTML('beforeend', joinedStrings)
+    womenButton.addEventListener("click", () => genderFilter(gender[1], shoesArray));
 
-    womenButton.addEventListener("click", function () {
-        console.log(theShoes.shoes.filter(shoe => shoe.gender === "woman"))
-    })
+    menButton.addEventListener("click", () => genderFilter(gender[0], shoesArray));
 
-    menButton.addEventListener("click", function () {
-        console.log(theShoes.shoes.filter(shoe => shoe.gender === "man"))
-    })
-
-
+    resetButton.addEventListener("click", () => removeHideClass());
 }
+
 initializePage();
 
 
-const header = document.createElement('div')
-header.id = "header";
-header.textContent = 'ShoeSzter';
-//header.style.background = "frontend\pictures\header_pic_v2.jpg";
-root.appendChild(header)
+
