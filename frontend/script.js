fa6eec47e246bdf8dca36b73cf7680a33af9e1af
Tímea Tamasi <103537849+DoomP1nky@users.noreply.github.com>
2023-02-09 10:15:13 +0100

    changed pictures, add style in scss, create elementsin script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,22 +1,14 @@
 console.log("Ez a srcipt file!")
 
 const root = document.getElementById('root');
-const womenButton = document.createElement('button');
-womenButton.textContent = "WOMEN'S";
-root.appendChild(womenButton);
-
-const menButton = document.createElement('button');
-menButton.textContent = "MEN'S";
-root.appendChild(menButton);
 
 // const selectSize = document.createElement()
 let shoeCount = 0;
 function shoeComponent(shoe) {
     shoeCount++;
     return `
-    <br>
     <div id=contID${shoeCount}>
-        <img id=imgID${shoeCount} src=${shoe.src} width=200 height=200>
+        <img id=imgID${shoeCount} src=${shoe.src} width=300 height=300>
         <div>${shoe.name}</div>
         <div>${shoe.price} Ft</div>
     </div>
@@ -49,4 +41,52 @@ const header = document.createElement('div')
 header.id = "header";
 header.textContent = 'ShoeSzter';
 //header.style.background = "frontend\pictures\header_pic_v2.jpg";
-root.appendChild(header)
+root.appendChild(header);
+
+// Create an image element
+let image = document.createElement("img");
+
+// Set the source, alt, and id attributes of the image
+image.src = "/pictures/header_pic.jpg";
+image.alt = "Header picture";
+image.id = "header-picture";
+
+// Append the image to the header div
+header.appendChild(image);
+
+const contentDiv = document.createElement('div');
+contentDiv.id = 'content-div';
+root.appendChild(contentDiv);
+
+const womenButton = document.createElement('button');
+womenButton.textContent = "WOMEN'S";
+womenButton.id = "woman-button";
+root.appendChild(womenButton);
+
+const menButton = document.createElement('button');
+menButton.textContent = "MEN'S";
+menButton.id = "men-button";
+root.appendChild(menButton);
+
+const cart = document.createElement('div');
+cart.classList.add('shopping-cart');
+
+const cartIcon = document.createElement('div');
+cartIcon.classList.add('cart-icon');
+cartIcon.innerHTML = '<i class="fa-regular fa-cart-shopping"></i>';
+
+const cartCount = document.createElement('div');
+cartCount.classList.add('cart-count');
+cartCount.innerText = '0';
+
+
+cart.appendChild(cartIcon);
+cart.appendChild(cartCount);
+
+root.appendChild(cart);
+
+/* const footer = document.createElement('div')
+footer.id = "footer";
+footer.textContent = 'Contact';
+//header.style.background = "frontend\pictures\header_pic_v2.jpg";
+root.appendChild(footer); */
